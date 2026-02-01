ddfa6fd8d3084d15f8c4f6109e9fd747864ca136
user01022026 <user01022026@proton.me>
2026-02-01 23:05:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/user01022026.github.io/",
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +19,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
